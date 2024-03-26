bcd538101c054f565b31b786e8c787d08bf9b416
JpedroA <joaopedropaulino@souunisuam.com.br>
2024-03-26 19:21:11 -0300

    executando exercicios

--- script.js
+++ script.js
@@ -4,8 +4,7 @@
 // let list : string[] = ["joao", "maria", "carla", "fabio"];
 // let list2 : Array<number> = [20,17,24];
 // let obj = { nome: "joao", estado: "RJ", idade: 23};
-const listAlunos : any[] = [{nome: "jessica", idade:22}, {nome:"pedro", idade: 24},{nome:"tirion", idade: 45}];
-
+var listAlunos = [{ nome: "jessica", idade: 22 }, { nome: "pedro", idade: 24 }, { nome: "tirion", idade: 45 }];
 // console.log(num);
 // console.log(str);
 // console.log(bool);
@@ -15,4 +14,4 @@ const listAlunos : any[] = [{nome: "jessica", idade:22}, {nome:"pedro", idade: 2
 // console.log(obj.estado);
 // console.log(obj.idade);
 console.log(listAlunos);
-console.log(listAlunos[1]);
+console.log(listAlunos[2]);
