7ac53cf277ee68b01b90f8e0dc7b9fc4713dbd59
JpedroA <joaopedropaulino@souunisuam.com.br>
2024-03-30 12:45:59 -0300

    funcoes simples

--- script.js
+++ script.js
@@ -5,6 +5,10 @@
 // let list2 : Array<number> = [20,17,24];
 // let obj = { nome: "joao", estado: "RJ", idade: 23};
 var listAlunos = [{ nome: "jessica", idade: 22 }, { nome: "pedro", idade: 24 }, { nome: "tirion", idade: 45 }];
+function media(num1, num2) {
+    return (num1 + num2) / 2;
+}
+var soma = function (numb1, numb2) { return numb1 + numb2; };
 // console.log(num);
 // console.log(str);
 // console.log(bool);
@@ -13,5 +17,9 @@ var listAlunos = [{ nome: "jessica", idade: 22 }, { nome: "pedro", idade: 24 }, 
 // console.log(obj);
 // console.log(obj.estado);
 // console.log(obj.idade);
+var somar = soma(29, 1);
+var result = media(20, 30);
+console.log(somar);
+console.log(result);
 console.log(listAlunos);
-console.log(listAlunos[2]);
+console.log(listAlunos[1]);
--- script.ts
+++ script.ts
@@ -6,6 +6,11 @@
 // let obj = { nome: "joao", estado: "RJ", idade: 23};
 const listAlunos : any[] = [{nome: "jessica", idade:22}, {nome:"pedro", idade: 24},{nome:"tirion", idade: 45}];
 
+function media(num1: number, num2: number): number {
+    return (num1 + num2)/2;
+}
+
+let soma = (numb1: number, numb2: number): number => numb1+numb2;
 // console.log(num);
 // console.log(str);
 // console.log(bool);
@@ -14,5 +19,9 @@ const listAlunos : any[] = [{nome: "jessica", idade:22}, {nome:"pedro", idade: 2
 // console.log(obj);
 // console.log(obj.estado);
 // console.log(obj.idade);
+let somar = soma(29,1);
+let result = media(20, 30);
+console.log(somar);
+console.log(result);
 console.log(listAlunos);
 console.log(listAlunos[1]);
